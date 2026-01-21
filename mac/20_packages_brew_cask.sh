20baddf7209976161f86f70a61d82be4a542d3b9
akhon <akhon@users.noreply.github.com>
2026-01-21 14:18:37 -0600

    Update 20_packages_brew_cask.sh

--- mac/20_packages_brew_cask.sh
+++ mac/20_packages_brew_cask.sh
@@ -1,41 +1,24 @@
 #!/usr/bin/env bash
 
 brew tap homebrew/cask
-
-brew install 1password --cask
-brew install adium --cask
-brew install appcleaner --cask
 brew install balenaetcher --cask
-brew install clipy --cask
-brew install daisydisk --cask
 brew install docker --cask
 brew install dropbox --cask
-brew install evernote --cask
-brew install gitup --cask
-brew install google-chrome --cask
 brew install gpg-suite --cask && sudo rm -rf /Library/Mail/Bundles/GPGMail.mailbundle
 brew install haptickey --cask
 brew install iterm2 --cask
 brew install keybase --cask
 brew install ngrok --cask
 brew install openoffice --cask
-brew install opera --cask
 brew install osxfuse --cask && brew install sshfs --cask
-brew install pgadmin4 --cask
 brew install pock --cask
 brew install pycharm-ce --cask
 brew install p4v --cask
-brew install scroll-reverser --cask
-brew install skype --cask
 brew install slack --cask
-brew install spotify --cask
 brew install spotmenu --cask
 brew install spectacle --cask
 brew install the-unarchiver --cask
 brew install telegram --cask
-brew install tor-browser --cask
-brew install tunnelbear --cask
-brew install tunnelblick --cask
 brew install visual-studio-code --cask
 brew install vlc --cask
 brew install yujitach-menumeters --cask
