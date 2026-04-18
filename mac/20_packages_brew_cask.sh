3d34182ac8cb5fb91ce0147f371c2585ad56d6b1
Andrey Khon <akhon@samodelkin.net>
2026-04-17 20:01:21 -0500

    muteme back

--- mac/20_packages_brew_cask.sh
+++ mac/20_packages_brew_cask.sh
@@ -4,5 +4,6 @@ brew install balenaetcher --cask
 brew install chatgpt --cask
 brew install docker-desktop --cask
 brew install iterm2 --cask
+brew install muteme --cask
 brew install tsh --cask
 brew install usr-sse2-rdm --cask
